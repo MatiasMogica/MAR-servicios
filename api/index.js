98b85decf1685e7dc091fd7d850be6f0e6eb16d9
MatiasMogica <matimogica@gmail.com>
2024-11-20 13:10:41 -0300

    tratando de deployar parte mil

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 module.exports = app; // Exporta la app para Vercel
 
 app.use(express.json());
